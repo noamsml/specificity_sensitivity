1f871dc44ff7b5dc8efde26645b1b12ae0eddb45
Noam Samuel <noam@stripe.com>
2020-05-12 17:06:59 -0700

    Add percent positive in population

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 (function() {
     let $ = document.getElementById.bind(document);
 
+    var lockPrevelance = true;
+
+    function getPrevelance(specificity, sensitivity) {
+        if (lockPrevelance) {
+            return $("prevelance").valueAsNumber / 100.0;
+        } else {
+            // STEVE'S EQUATION: prevelance = (positive tests + specificity - 1)/(sensitivity + specificity - 1)
+
+            // positive_tests = false_positive_rate + true_positive_rate
+            // positive_tests = (1.0 - specificity) * (1.0 - prevelance) + sensitivity * prevelance
+            // positive_tests =  1  + specificity * prevelacne - specificity - prevelance + sensitivity * prevelance
+            // = 1 + prevelance (spec + sens - 1) - specificity
+            //
+            // positive_tests + specificity - 1 = prevelance (spec + sens - 1)
+            // prevelance = (positive_tests + specificity - 1) / (spec + sens - 1)
+
+            let positive_tests = $("positive_percent").valueAsNumber / 100.0;
+
+            return (positive_tests + specificity - 1.0)/(sensitivity + specificity - 1.0);
+        }
+    }
+
     function calculateOutputs() {
         let sensitivity = $("sensitivity").valueAsNumber / 100.0;
         let specificity = $("specificity").valueAsNumber / 100.0;
-        let prevelance = $("prevelance").valueAsNumber / 100.0;
+        let prevelance = getPrevelance(specificity, sensitivity);
 
         let true_positive_rate = sensitivity * prevelance;
         let true_negative_rate = specificity * (1.0 - prevelance);
@@ -17,13 +39,28 @@
 
         $("positive_likelihood").value = (probability_given_positive * 100).toFixed(2).toString();
         $("negative_likelihood").value = (probability_given_negative * 100).toFixed(2).toString();
+
+        if (lockPrevelance) {
+            let positive_tests = true_positive_rate + false_positive_rate;
+            $("positive_percent").value = (positive_tests * 100).toFixed(2).toString();
+        } else {
+            $("prevelance").value = (prevelance * 100).toFixed(2).toString();
+        }
     }
 
 
     document.addEventListener("DOMContentLoaded", () => {
         $("sensitivity").addEventListener("change", calculateOutputs);
         $("specificity").addEventListener("change", calculateOutputs);
-        $("prevelance").addEventListener("change", calculateOutputs);
+        $("prevelance").addEventListener("change", () => {
+            lockPrevelance = true;
+            calculateOutputs();
+        });
+
+        $("positive_percent").addEventListener("change", () => {
+            lockPrevelance = false;
+            calculateOutputs();
+        });
         calculateOutputs();
     });
 })()
